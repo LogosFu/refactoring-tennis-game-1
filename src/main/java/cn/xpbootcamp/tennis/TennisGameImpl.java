116ba0956dbcdd2907444a18c5905b17bae2870f
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-03-08 15:37:04 +0800

    简单清理

--- src/main/java/cn/xpbootcamp/tennis/TennisGameImpl.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGameImpl.java
@@ -3,8 +3,8 @@ package cn.xpbootcamp.tennis;
 
 public class TennisGameImpl implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Point = 0;
+    private int player2Point = 0;
     private String player1Name;
     private String player2Name;
 
@@ -14,43 +14,50 @@ public class TennisGameImpl implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            player1Point++;
+        } else {
+            player2Point++;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        if (player1Point == player2Point) {
+            switch (player1Point) {
                 case 0:
-                    score = "Love-All";
+                    score = "Love" + "-All";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score = "Fifteen" + "-All";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score = "Thirty" + "-All";
                     break;
                 default:
                     score = "Deuce";
                     break;
 
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+        } else if (player1Point >= 4 || player2Point >= 4) {
+            int minusResult = player1Point - player2Point;
+            if (minusResult == 1) {
+                score = "Advantage " + player1Name;
+            } else if (minusResult == -1) {
+                score = "Advantage " + player2Name;
+            } else if (minusResult >= 2) {
+                score = "Win for " + player1Name;
+            } else {
+                score = "Win for " + player2Name;
+            }
         } else {
+            int tempScore = 0;
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
+                if (i == 1) {
+                    tempScore = player1Point;
+                } else {
                     score += "-";
-                    tempScore = m_score2;
+                    tempScore = player2Point;
                 }
                 switch (tempScore) {
                     case 0:
